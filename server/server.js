31f24b3305a45045e5044fe24ed3891b27c729c7
Joe <s2802053@griffithuni.edu.au>
2019-09-17 23:12:00 +1000

    Added import for socket.io Added middleware function to expose sockets io object on requests Updated cors to allow requests from front-end

--- server/server.js
+++ server/server.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const mongoose = require('mongoose');
-
+let io = require('socket.io')(http);
+io = require(__dirname + '/sockets/addNamespaceForChannels')(io);
 const api_route = require('./routes/routeHandler');
 
 mongoose.connect('mongodb://localhost:27017/chat-client', {
@@ -20,12 +21,21 @@ app.use(session({
     secret: "s2802053-3813ict",
     cookie: {
         maxAge: 1000 * 60 * 60 * 2,
-        sameSite: true
+        //sameSite: true
     }
 }));
-app.use(cors());
+app.use(cors({credentials: true, origin: 'http://localhost:4200'}));
 app.use(bodyParser.json());
+
+// add a reference to the io object to each request
+app.use((req, res, next) => {
+    req.io = io;
+    next();
+});
+
 app.use('/api', api_route);
+
+
 
 http.listen(3000, () => {
     console.log("Server listening on port 3000");
