e681aab0f792d693c3e5698de3030c8198174719
Joe <s2802053@griffithuni.edu.au>
2019-09-17 23:10:12 +1000

    Changed name of 'user' property to 'username' Changed name of 'content' property to 'message' Changed type of username property from ObjectId to String

--- server/models/schemas/message.js
+++ server/models/schemas/message.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-    user: {type: Schema.ObjectId},
-    timestamp: {type: Date, default: Date.now},
-    content: {type: String}
+    username: {type: String},
+    timestamp: {type: String},
+    message: {type: String}
 });
 
 module.exports = messageSchema;
